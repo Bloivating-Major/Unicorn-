6246faea68fb5775a94d3310a5a49ec9fcc42558
Sambhav <sambhav12wakhariya@gmail.com>
2025-05-15 23:30:00 +0530

    Features Data Added

---
+++ src/lib/constants/features.js
@@ -0,0 +1,23 @@
+export const features = [
+  {
+    title: "Join a Community of Passionate Chess Learners and Players",
+    description:
+      "Experience the joy of chess while building critical thinking skills.",
+    buttonText: "Join",
+    iconUrl: "/src/assets/pawn.svg",
+  },
+  {
+    title: "Book Your First Class Today and Start Your Chess Journey",
+    description:
+      "Our classes cater to all skill levels, from beginners to advanced players.",
+    buttonText: "Book",
+    iconUrl: "/src/assets/rook.svg",
+  },
+  {
+    title: "Discover the Exciting World of Chess Tournaments and Events",
+    description:
+      "Join us for thrilling competitions that foster growth and camaraderie.",
+    buttonText: "Learn",
+    iconUrl: "/src/assets/Knight.svg",
+  },
+];
