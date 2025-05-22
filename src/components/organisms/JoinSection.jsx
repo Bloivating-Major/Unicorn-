b22669dff7d1ed417fb163a4b080fd6d489447c6
Sambhav <sambhav12wakhariya@gmail.com>
2025-05-22 23:30:00 +0530

    Join Section Added

---
+++ src/components/organisms/JoinSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../atoms/Button";
+
+const JoinSection = () => {
+  return (
+    <section className="bg-background-dark text-white py-16  max-sm:px-5">
+      <div className="container mx-auto">
+        <h2 className="text-5xl mb-2">Join Our Academy</h2>
+        <p className="mb-6 py-5">
+          Unlock your potential and master the game of chess with our expert coaching and community.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default JoinSection;
