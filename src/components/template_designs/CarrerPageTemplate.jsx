48ab7ecc51d0e524a6838feb93f907bd463e4aaa
Sambhav <sambhav12wakhariya@gmail.com>
2025-05-24 23:30:00 +0530

    Carrer Page Template Added

---
+++ src/components/template_designs/CarrerPageTemplate.jsx
@@ -0,0 +1,23 @@
+import JoinSection from '../organisms/JoinSection'
+import OpportunitiesSection from '../organisms/OpportunitiesSection'
+import EmpowerSection from '../organisms/EmpowerSection'
+import TeamSection from '../organisms/TeamSection'
+import HiringSection from '../organisms/HiringSection'
+import GetInTouchForm from '../organisms/GetInTouch'
+import CTASection from '../organisms/CTASection'
+
+const CarrerPageTemplate = () => {
+  return (
+    <main className="bg-background-dark text-white min-h-screen px-6 py-12 font-poppins">
+        <JoinSection />
+        <OpportunitiesSection />
+        <EmpowerSection />
+        <TeamSection />
+        <HiringSection />
+        <GetInTouchForm />
+        <CTASection />
+    </main>
+  )
+}
+
+export default CarrerPageTemplate
