a5d90a421fb412bf0f9cfaca3015dd80ddd010b4
Sambhav <sambhav12wakhariya@gmail.com>
2025-05-26 23:30:00 +0530

    Main App Added

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/organisms/Navigation";
+import Gallery from "./pages/GalleryPage";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/organisms/Footer";
+import Contact from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import CarrerPage from "./pages/CarrerPage";
+
+const App = () => {
+  return (
+    <Router>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route  path="/gallery"  element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/carrer" element={<CarrerPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+};
+
+export default App;
