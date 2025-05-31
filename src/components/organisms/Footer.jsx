95714493c4dba48b0eb1ef1ca3556698a186d0b1
Sambhav <sambhav12wakhariya@gmail.com>
2025-05-31 23:30:00 +0530

    Footer Updated

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -8,14 +8,14 @@ const Footer = () => {
         <div className="flex justify-center py-5">
           <NavLink className="text-center">Logo</NavLink>
         </div>
-        <div className="flex justify-center md:gap-8 gap-4 mb-4 max-sm:flex-col max-sm:text-center font-bold">
+        <div className="flex justify-center md:gap-16 gap-4 mb-4 max-sm:flex-col max-sm:text-center font-medium">
           <NavLink href="#">Join Us</NavLink>
           <NavLink href="#">Contact Us</NavLink>
           <NavLink href="#">Student Portal</NavLink>
         </div>
-        <div className="text-center">
+        <div className="flex items-center justify-between py-3 border-t border-gray-700 max-sm:flex-col-reverse max-sm:gap-3">
           <p>© 2025 Chess Academy. All rights reserved.</p>
-          <div className="flex justify-center space-x-4 mt-4 font-bold " >
+          <div className="flex justify-center md:space-x-4 font-medium max-sm:flex-col max-sm:text-center max-sm:gap-2" >
             <NavLink href="#" className="text-sm border-b">Privacy Policy</NavLink>
             <NavLink href="#" className="text-sm border-b">Terms of Use</NavLink>
             <NavLink href="#" className="text-sm border-b">Cookie Policy</NavLink>
