170b28d482eae2e574d70c1c1efe3e77d06eab1f
Sambhav <sambhav12wakhariya@gmail.com>
2025-06-02 23:30:00 +0530

    Pricing Plans Added

---
+++ src/lib/constants/pricingPlans.js
@@ -0,0 +1,32 @@
+export const pricingPlans = [
+  {
+    name: "Basic Plan",
+    price: "$19/mo",
+    features: [
+      "Access to beginner classes",
+      "Weekly practice sessions",
+      "Online resources included",
+    ],
+  },
+  {
+    name: "Business Plan",
+    price: "$29/mo",
+    features: [
+      "All Basic features",
+      "Monthly tournaments access",
+      "Personal coaching sessions",
+      "Exclusive member events",
+    ],
+  },
+  {
+    name: "Enterprise Plan",
+    price: "$49/mo",
+    features: [
+      "All Business features",
+      "Advanced strategy workshops",
+      "Priority support access",
+      "Custom training plans",
+      "Monthly analysis reports",
+    ],
+  },
+];
