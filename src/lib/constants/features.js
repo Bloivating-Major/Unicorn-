9a29521456823b724d460b601941ff44b9fbd071
Sambhav <sambhav12wakhariya@gmail.com>
2025-06-02 23:30:00 +0530

    Features Modified

--- src/lib/constants/features.js
+++ src/lib/constants/features.js
@@ -4,20 +4,20 @@ export const features = [
     description:
       "Experience the joy of chess while building critical thinking skills.",
     buttonText: "Join",
-    iconUrl: "/src/assets/pawn.svg",
+    iconUrl: "/src/assets/images/pawn.svg",
   },
   {
     title: "Book Your First Class Today and Start Your Chess Journey",
     description:
       "Our classes cater to all skill levels, from beginners to advanced players.",
     buttonText: "Book",
-    iconUrl: "/src/assets/rook.svg",
+    iconUrl: "/src/assets/images/rook.svg",
   },
   {
     title: "Discover the Exciting World of Chess Tournaments and Events",
     description:
       "Join us for thrilling competitions that foster growth and camaraderie.",
     buttonText: "Learn",
-    iconUrl: "/src/assets/Knight.svg",
+    iconUrl: "/src/assets/images/Knight.svg",
   },
 ];
