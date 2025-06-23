8c2a234270a9d9934825bbc637ddcc5804158117
Bloivating-Major <sambhav12wakhariya@gmail.com>
2025-06-22 23:30:00 -0400

    About Hero Section Modified

--- src/components/organisms/AboutHero.jsx
+++ src/components/organisms/AboutHero.jsx
@@ -1,20 +1,21 @@
+import Button from "../atoms/Button";
 import FloatingChessPieces from "../molecules/FloatingChessPieces";
 
 const AboutHero = () => {
   return (
-    <section className="container mx-auto bg-background-dark text-white px-6 py-20 md:px-20 lg:px-32 flex flex-col md:flex-row items-center justify-between rounded-2xl shadow-xl">
-      <div className="max-w-xl space-y-5">
-        <h1 className="text-4xl md:text-5xl font-bold">Unlock Your Potential</h1>
-        <p className="text-lg text-gray-300">
+    <section className="section-wrapper flex flex-col md:flex-row items-center justify-between max-sm:px-5">
+      <div className=" space-y-5">
+        <h1 className="heading-text">Unlock Your Potential</h1>
+        <p className="body-text">
           Join our Chess Academy to enhance your skills and become a master strategist in this game.
         </p>
         <div className="flex gap-4 mt-6">
-          <button className="bg-indigo-200 text-black px-6 py-2 rounded-md font-semibold shadow hover:opacity-90 transition">
+          <Button variant="primary" size="medium" >
             Join
-          </button>
-          <button className="bg-white text-black px-6 py-2 rounded-md font-semibold shadow hover:opacity-90 transition">
+          </Button>
+          <Button variant="secondary" size="medium">
             Learn More
-          </button>
+          </Button>
         </div>
       </div>
 
