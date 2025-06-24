5b1412d7039d27629703f55a6a5270eb13542c5d
Bloivating-Major <sambhav12wakhariya@gmail.com>
2025-06-23 23:30:00 -0400

    Join Academy Section Modified

--- src/components/organisms/JoinAcademy.jsx
+++ src/components/organisms/JoinAcademy.jsx
@@ -2,25 +2,25 @@ import Button from "../atoms/Button";
 
 const JoinAcademyCTA = () => {
   return (
-    <section className="container mx-auto bg-background-dark text-white px-6 py-20 md:px-20 lg:px-32">
+    <section className="section-wrapper max-sm:px-5">
       <div className="grid md:grid-cols-2 items-center gap-12">
         {/* Text Section */}
-        <div>
-          <h2 className="text-3xl md:text-4xl font-bold">
+        <div className="flex flex-col gap-3">
+          <h2 className="heading-text">
             Unlock Your Potential: <br />
             Join Our Chess Academy Community Today!
           </h2>
-          <p className="text-gray-300 mt-4">
+          <p className="body-text">
             As a part of our vibrant academy, you&apos;ll gain access to expert
             coaching, exclusive tournaments, and a strong support system. Start
             your journey toward chess mastery today.
           </p>
 
-          <div className="mt-6 flex gap-4">
-            <Button variant="primary" size="large">
+          <div className="flex gap-4">
+            <Button variant="primary" size="medium">
               Join Now
             </Button>
-            <Button variant="secondary" size="large">
+            <Button variant="secondary" size="medium">
               Learn More
             </Button>
           </div>
