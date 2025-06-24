f6f27ad0955b9fa144a77e1709110f8dc0e3cec3
Bloivating-Major <sambhav12wakhariya@gmail.com>
2025-06-23 23:30:00 -0400

    Gallery Modified

--- src/components/organisms/GalleryHero.jsx
+++ src/components/organisms/GalleryHero.jsx
@@ -1,6 +1,9 @@
+import { section } from "framer-motion/client";
+
 const GalleryHero = () => {
   return (
-    <div className="container mx-auto max-w-6xl flex flex-col md:flex-row items-start md:items-center justify-between mb-16 gap-8">
+    <section className="section-wrapper">
+    <div className="flex flex-col md:flex-row items-start md:items-center justify-between mb-16 gap-8">
       <h1 className="text-4xl md:text-5xl ">
         Our Chess Gallery
       </h1>
@@ -10,6 +13,7 @@ const GalleryHero = () => {
         skill, and the vibrant community we foster.
       </p>
     </div>
+    </section>
   );
 };
 
