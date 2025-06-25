97af3d72fc2909907fab0ab5f812142cca1c20cb
Bloivating-Major <sambhav12wakhariya@gmail.com>
2025-06-24 23:30:00 -0400

    Mission Intro Section Modified

--- src/components/organisms/MissionIntro.jsx
+++ src/components/organisms/MissionIntro.jsx
@@ -1,12 +1,12 @@
 const MissionIntro = () => {
   return (
-    <section className="container mx-auto  px-6 py-20 md:px-20 lg:px-32 bg-background-dark text-white flex flex-col-reverse md:flex-row items-center gap-10">
+    <section className="section-wrapper flex flex-col-reverse md:flex-row items-center gap-10 max-sm:px-5">
       {/* Text Content */}
       <div className="w-full md:w-1/2 space-y-6">
-        <h2 className="text-2xl md:text-3xl font-semibold">
+        <h2 className="heading-text">
           Founded in 2023, our academy is dedicated to nurturing chess talent.
         </h2>
-        <p className="text-lg leading-relaxed text-gray-300">
+        <p className="body-text">
           Our Chess Academy was established to provide an exceptional curriculum
           for students worldwide. We focus on personalized instruction, community engagement,
           and nurturing passion for the long-term.
@@ -14,11 +14,15 @@ const MissionIntro = () => {
       </div>
 
       {/* Image */}
-        <div className="md:w-1/2 p-4 py-8 flex items-center justify-center">
-          <div className="md:w-[600px]  md:h-[600px] rounded-md overflow-hidden  ">
-            <img src="/src/assets/images/bobby600x640.webp" alt="Bobby Fischer" className="rounded-md" />
-            </div> {/* Placeholder for image */}
+      <div className="md:w-1/2 flex items-center justify-center">
+        <div className="w-full md:w-[600px] max-w-full rounded-md overflow-hidden">
+          <img
+            src="/src/assets/images/bobby600x640.webp"
+            alt="Bobby Fischer"
+            className="rounded-md w-full h-auto object-cover"
+          />
         </div>
+      </div>
     </section>
   );
 };
